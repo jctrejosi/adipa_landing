530c3fa1644259f24e1aaf8e1f57f7480580ae8e
Juan Carlos Trejos <jctrejosi@unal.edu.co>
2026-04-19 00:21:07 -0500

    add jest in version_a

---
+++ version_a/jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+    "\\.(css|scss|sass|module.css)$": "identity-obj-proxy",
+  },
+};
+
+export default config;
